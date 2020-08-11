e1fcc1ee9299fab46689c17276f23fcc9d0775bc
Bahriye Begum Ultan Gungor <begumultan@gmail.com>
2020-08-11 17:28:05 +0300

    First commit

---
+++ Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+var myNumber = 1
+myNumber = myNumber + 1
+myNumber += 1
+myNumber -= 1
+
+
+
+// WHILE LOOP
+var number = 0
+while number <= 10 {
+    print(number)
+    number += 1
+}
+
+var characterAlive = true
+while characterAlive == true {
+    print("character alive")
+    characterAlive = false
+}
+
+3 < 5
+5 < 3
+5 > 3
+5 == 5
+5 != 4
+
+
+
+
+// FOR LOOP
+ var myFruitArray = ["Banana", "Apple", "Orange"]
+print(myFruitArray[0])
+print(myFruitArray[1])
+print(myFruitArray[2])
+for fruit in myFruitArray {
+    print(fruit)
+}
+
+var myNumbers = [10,20,30,40,50,60]
+for num in myNumbers {
+    print(num / 5)
+}
+
+for myNewInteger in 1 ... 5{
+    print(myNewInteger*5)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
